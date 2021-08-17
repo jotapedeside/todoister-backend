3e053cbeec53765160ed85986b92f05f9ee8ae3c
jotapedeside <jotapedeside@gmail.com>
2021-08-17 01:22:17 -0300

    feature(app): POST method created

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const port = 3000
-const mongoose = require('mongoose');
-const db = require('./db');
+var db = require('./db');
+var Task = db.Mongoose.model('tasks', db.taskSchema, 'tasks');
 
-router.get('/', (req, res) => {
+/* GET home */
+app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+/* POST one task */
+app.post('/task', function(req, res, next){
+  Task.create(req.body, (error) => {
+    if(error) {
+      res.status(500).json({ error: error.message });
+      res.end();
+      return;
+    }
+    res.json(req.body);
+    res.end();
+    return;
+  })
+})
+
+app.post('/task', function(req, res, next){
+  Task.create(req.body, (error) => {
+    if(error) {
+      res.status(500).json({ error: error.message });
+      res.end();
+      return;
+    }
+    res.json(req.body);
+    res.end();
+    return;
+  })
 })
 
 app.listen(port, () => {
