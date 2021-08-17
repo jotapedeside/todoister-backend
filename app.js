358b5b6a7fbe0cd0f8168a0733b18ced2361fdc4
jotapedeside <jotapedeside@gmail.com>
2021-08-17 01:22:52 -0300

    feature(app): GET all tasks method created

--- app.js
+++ app.js
@@ -39,6 +39,30 @@ app.post('/task', function(req, res, next){
   })
 })
 
+/* GET all tasks. */
+app.get('/task/:page/:pageLimit/:sort', function(req, res, next) {
+  let perPage = req.params.pageLimit * 1;
+  let currentPage = Math.max(0, req.params.page-1);
+  let sortedBy = req.params.sort;
+  
+  let totalObjs = 0;  
+  
+  Task.find({ status: false }).limit(perPage).skip(perPage * currentPage).sort({ createdAt : sortedBy}).exec(function(error,docs) {
+    if(error) {
+      res.status(500).json({error: error.message});
+      res.end();
+      return;
+    }
+
+    Task.countDocuments({status: false},function(err, count) {
+      if(err) totalObjs = -1;
+        totalObjs = count;
+        res.json({page: currentPage + 1, length: totalObjs, page_size: perPage , data: docs});
+        res.end();
+      });
+    });  
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
