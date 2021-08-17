38dd7646e32f8d5d64fe03f6d8a26f9104992207
jotapedeside <jotapedeside@gmail.com>
2021-08-17 01:21:51 -0300

    feature(db): taskSchema created

--- db.js
+++ db.js
@@ -8,5 +8,27 @@ mongoose.connect(process.env.DB_HOST, {
 }).then(() => {
     console.log("Connected with MongoDB!");
 }).catch((error) => {
+    console.log(error);
     console.log("Error: Cannot Connect to MongoDB!!!");
 });
+
+var taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status:{
+        type: Boolean,
+        default: false
+    }
+},  {
+    collection: 'tasks',
+    timestamps: true
+    }
+);
+
+module.exports = {Mongoose: mongoose, taskSchema: taskSchema}
